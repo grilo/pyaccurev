adabaf69841468ddbb45f11c711874a0951dd16f
grilo <joao.grilo@gmail.com>
2017-06-25 03:41:50 +0200

    Rename file, cleanup the contents.

---
+++ examples.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import accurev.client
+client = accurev.client.Client()
+
+print 'Stream count:', len(client.depots['OFFICE'].streams)
+
+for stream in client.depots['OFFICE'].streams:
+    print stream
+
+print client.depots['OFFICE'].streams['feature_branch_2.0']
+
+
+print depot.streams['feature_branch_2.0'].workspaces
+print depot.streams['feature_branch_2.0'].issues
+
+print depot.streams['feature_branch_2.0'].issues.values()[0].developers
+
+for e in client.depots['OFFICE'].streams['feature_branch_2.0'].issues['JIRA-37'].elements.values():
+    if e.missing == "false":
+        print e
+
+ Promote by issue
+for issue in client.depots['OFFICE'].streams['feature_branch_2.0'].issues.values():
+    if issue.IDJira == 'JIRA-37':
+        out, err = client.promote_issues([issue.issueNum], issue.stream.name, 'feature_branch_sandbox')
+        print out
+
+for dep in client.depots['OFFICE'].streams['feature_branch_2.0'].issues['JIRA-37'].dependencies.values():
+    print dep
